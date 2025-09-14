5482ea3c82b6885b62fb2ecbd66eeb0ba7959c08
Ludger Solbach <soulman@soulspace.org>
2025-09-14 07:10:35 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Provides an IBM Quantum Backend for QClojure."
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.1"]
-                 [org.clojure/data.json "2.4.0"]
-                 [org.clojure/spec.alpha "0.3.218"]
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [org.clojure/data.json "2.5.1"]
+                 [org.clojure/spec.alpha "0.5.238"]
                  [org.babashka/http-client "0.4.23"]
                  [com.github.oliyh/martian "0.2.0"]
                  [com.github.oliyh/martian-babashka-http-client "0.2.0"]
-                 [org.soulspace/qclojure "0.15.0"]]
+                 [org.soulspace/qclojure "0.16.0"]]
 
   :scm {:name "git" :url "https://github.com/lsolbach/qclojure-ibmq"}
   :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
