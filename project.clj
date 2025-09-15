d4bcf7bcf2e7fc545ab28cbbed41da4af3dc8a6a
Ludger Solbach <soulman@soulspace.org>
2025-09-15 19:45:20 +0200

    updated qclojure version to 0.17.0

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.babashka/http-client "0.4.23"]
                  [com.github.oliyh/martian "0.2.0"]
                  [com.github.oliyh/martian-babashka-http-client "0.2.0"]
-                 [org.soulspace/qclojure "0.16.0"]]
+                 [org.soulspace/qclojure "0.17.0"]]
 
   :scm {:name "git" :url "https://github.com/lsolbach/qclojure-ibmq"}
   :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
